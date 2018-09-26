6e6746a056c733c1cc08103a18f6d838ca02ead0
Scott Crespo <screspo@highcornergroup.com>
2018-09-26 15:44:09 -0400

    cpu units helper method

--- laticore/metricsets/__init__.py
+++ laticore/metricsets/__init__.py
@@ -1,1 +1,2 @@
 from laticore.metricsets.metricsets import *
+from laticore.metricsets.cpu import create_cpu_units_metricset
---
+++ laticore/metricsets/cpu.py
@@ -0,0 +1,28 @@
+import numpy as np
+from collections import OrderedDict
+from laticore.metricsets import SupervisedTimeSeriesMetricSet
+
+def create_cpu_units_metricset(utilization_ms:SupervisedTimeSeriesMetricSet,
+    instnace_count_ms:SupervisedTimeSeriesMetricSet) -> SupervisedTimeSeriesMetricSet:
+    # convert instance metricset data into ordered dict
+    data = {}
+    for i,j in enumerate(instance_count_ms.X.ravel()):
+        data[j] = instance_count_ms.Y[i][0]
+
+    data = OrderedDict(sorted(data.items()))
+
+    # iterate through utilization_ms and multiply instance counts to get
+    # cpu unit consumed
+    new_y = np.zeros_like(utilization_ms.X)
+    default_y = np.average(instance_count_ms.Y)
+    for l, m in enumerate(utilization_ms.X.ravel()):
+        new_y[l][0] = utilization_ms.Y[l][0] * data.get(m, default_y)
+
+    # create a new metricset cpu * instance_count
+    utilization_ms = SupervisedTimeSeriesMetricSet(
+        X=utilization_ms.X,
+        Y=new_y,
+        Y_norm_buffer=float(utilization_ms.Y_norm_buffer),
+        Y_norm_coefficient = utilization_ms.Y_norm_coefficient,
+        lookback = utilization_ms.lookback,
+    )
